a3661aa8858f9deb2635a8c4f08d996c4a7c0877
Kelly C Santos <kellycristinasntos@gmail.com>
2021-12-22 22:33:04 -0300

    adicionada função onde toda vez que o usuario seleciona um link o menu é recolhido

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let btnMenu = document.getElementsByClassName('btn-menu')[0].addEventListener('click',exibirMenu)
 let menu = document.getElementsByClassName('menu')[0]
 
+
 function exibirMenu(){
     if(menu.classList.contains('menu-aberto')){
         menu.classList.remove('menu-aberto')
@@ -11,3 +12,62 @@ function exibirMenu(){
         menu.classList.add('menu-aberto')
     }
 }
+
+
+let itemMenu1 = document.getElementsByClassName('item-menu')[0].addEventListener('click',fecharMenu1)
+function fecharMenu1(){
+    if(menu.classList.contains('menu-aberto')){
+        menu.classList.remove('menu-aberto')
+        menu.classList.add('menu-fechado')
+    }else{
+        menu.classList.remove('menu-fechado')
+        menu.classList.add('menu-aberto')
+    }
+}
+
+let itemMenu2 = document.getElementsByClassName('item-menu')[1].addEventListener('click',fecharMenu2)
+function fecharMenu2(){
+    if(menu.classList.contains('menu-aberto')){
+        menu.classList.remove('menu-aberto')
+        menu.classList.add('menu-fechado')
+    }else{
+        menu.classList.remove('menu-fechado')
+        menu.classList.add('menu-aberto')
+    }
+}
+
+let itemMenu3 = document.getElementsByClassName('item-menu')[2].addEventListener('click',fecharMenu3)
+function fecharMenu3(){
+    if(menu.classList.contains('menu-aberto')){
+        menu.classList.remove('menu-aberto')
+        menu.classList.add('menu-fechado')
+    }else{
+        menu.classList.remove('menu-fechado')
+        menu.classList.add('menu-aberto')
+    }
+}
+
+let itemMenu4 = document.getElementsByClassName('item-menu')[3].addEventListener('click',fecharMenu4)
+function fecharMenu4(){
+    if(menu.classList.contains('menu-aberto')){
+        menu.classList.remove('menu-aberto')
+        menu.classList.add('menu-fechado')
+    }else{
+        menu.classList.remove('menu-fechado')
+        menu.classList.add('menu-aberto')
+    }
+}
+
+let itemMenu5 = document.getElementsByClassName('item-menu')[4].addEventListener('click',fecharMenu5)
+function fecharMenu5(){
+    if(menu.classList.contains('menu-aberto')){
+        menu.classList.remove('menu-aberto')
+        menu.classList.add('menu-fechado')
+    }else{
+        menu.classList.remove('menu-fechado')
+        menu.classList.add('menu-aberto')
+    }
+}
+
+
+
