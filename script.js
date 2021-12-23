b08626ef281cca1114f66c3401febe23c3edba1d
Kelly C Santos <kellycristinasntos@gmail.com>
2021-12-22 22:38:04 -0300

    reutilizei a função que é chamada pelo botão do menu fecha-lo toda vez que um link da navegação é clicado

--- script.js
+++ script.js
@@ -16,57 +16,27 @@ function exibirMenu(){
 
 let itemMenu1 = document.getElementsByClassName('item-menu')[0].addEventListener('click',fecharMenu1)
 function fecharMenu1(){
-    if(menu.classList.contains('menu-aberto')){
-        menu.classList.remove('menu-aberto')
-        menu.classList.add('menu-fechado')
-    }else{
-        menu.classList.remove('menu-fechado')
-        menu.classList.add('menu-aberto')
-    }
+   exibirMenu()
 }
 
 let itemMenu2 = document.getElementsByClassName('item-menu')[1].addEventListener('click',fecharMenu2)
 function fecharMenu2(){
-    if(menu.classList.contains('menu-aberto')){
-        menu.classList.remove('menu-aberto')
-        menu.classList.add('menu-fechado')
-    }else{
-        menu.classList.remove('menu-fechado')
-        menu.classList.add('menu-aberto')
-    }
+    exibirMenu()
 }
 
 let itemMenu3 = document.getElementsByClassName('item-menu')[2].addEventListener('click',fecharMenu3)
 function fecharMenu3(){
-    if(menu.classList.contains('menu-aberto')){
-        menu.classList.remove('menu-aberto')
-        menu.classList.add('menu-fechado')
-    }else{
-        menu.classList.remove('menu-fechado')
-        menu.classList.add('menu-aberto')
-    }
+    exibirMenu()
 }
 
 let itemMenu4 = document.getElementsByClassName('item-menu')[3].addEventListener('click',fecharMenu4)
 function fecharMenu4(){
-    if(menu.classList.contains('menu-aberto')){
-        menu.classList.remove('menu-aberto')
-        menu.classList.add('menu-fechado')
-    }else{
-        menu.classList.remove('menu-fechado')
-        menu.classList.add('menu-aberto')
-    }
+    exibirMenu()
 }
 
 let itemMenu5 = document.getElementsByClassName('item-menu')[4].addEventListener('click',fecharMenu5)
 function fecharMenu5(){
-    if(menu.classList.contains('menu-aberto')){
-        menu.classList.remove('menu-aberto')
-        menu.classList.add('menu-fechado')
-    }else{
-        menu.classList.remove('menu-fechado')
-        menu.classList.add('menu-aberto')
-    }
+    exibirMenu()
 }
 
 
